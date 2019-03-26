fad04531a3e18f7128dd1fb8df7b3490bb8900bb
Joseph Morant Navarro <joseph.morantnavarro@gmail.com>
2019-03-26 23:22:06 +0100

    Setting home content

--- src/components/app-home/app-home.e2e.ts
+++ src/components/app-home/app-home.e2e.ts
@@ -9,11 +9,4 @@ describe('app-home', () => {
     expect(element).toHaveClass('hydrated');
   });
 
-  it('contains a "Profile Page" button', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<app-home></app-home>');
-
-    const element = await page.find('app-home ion-content ion-button');
-    expect(element.textContent).toEqual('Profile page');
-  });
 });
--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -15,14 +15,7 @@ export class AppHome {
       </ion-header>,
 
       <ion-content padding>
-        <p>
-          Welcome to the PWA Toolkit. You can use this starter to build entire
-          apps with web components using Stencil and ionic/core! Check out the
-          README for everything that comes in this starter out of the box and
-          check out our docs on <a href="https://stenciljs.com">stenciljs.com</a> to get started.
-        </p>
-
-        <ion-button href="/profile/ionic" expand="block">Profile page</ion-button>
+        <p>If you get lost, the <a target="_blank" rel="noopener" href="https://stenciljs.com/docs/introduction">docs</a> will be your guide.</p>
       </ion-content>
     ];
   }
