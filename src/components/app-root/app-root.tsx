020a2bb5a8806b6cecc6b47ad731692e1682bfbc
Joseph Morant Navarro <joseph.morantnavarro@gmail.com>
2019-03-27 16:10:21 +0100

    Adding firestore

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -1,6 +1,7 @@
 import { Component } from '@stencil/core';
 import { firebaseConfig } from '../../helpers/config';
-import firebase from 'firebase/app';
+import firebase from '@firebase/app';
+import '@firebase/firestore';
 
 @Component({
   tag: 'app-root',
