706fb0e8c3bf8ad5339d86d0cea57c1dc4910a1b
Joseph Morant Navarro <joseph.morantnavarro@gmail.com>
2019-03-27 17:02:39 +0100

    Adding firebase auth, firestore and storage

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -1,6 +1,9 @@
 import { Component } from '@stencil/core';
 import { firebaseConfig } from '../../helpers/config';
 import firebase from '@firebase/app';
+import '@firebase/auth';
+import '@firebase/firestore';
+import '@firebase/storage';
 
 @Component({
   tag: 'app-root',
